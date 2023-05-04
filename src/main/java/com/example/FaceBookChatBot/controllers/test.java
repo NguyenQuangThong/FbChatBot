658984fd3c639c0b95cda9a9b560878a73acf536
NguyenQuangThong <thongnguyen120600@gmail.com>
2023-05-04 15:00:42 +0700

    Test GPT

--- src/main/java/com/example/FaceBookChatBot/controllers/test.java
+++ src/main/java/com/example/FaceBookChatBot/controllers/test.java
@@ -1,21 +1,31 @@
 package com.example.FaceBookChatBot.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.messenger4j.Messenger;
+import com.github.messenger4j.exception.MessengerVerificationException;
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
+import static com.github.messenger4j.Messenger.*;
 
 @CrossOrigin("*")
 @RestController
+@RequestMapping("/webhook")
 public class test {
 
     @Autowired
@@ -27,7 +37,31 @@ public class test {
     @Value("${messenger4j.pageAccessToken}")
     private String pageAccessToken;
 
-    @PostMapping("/webhook")
+    private static final Logger logger = LoggerFactory.getLogger(WebhookController.class);
+
+    private final Messenger messenger;
+
+    @Autowired
+    public test(final Messenger messenger) {
+        this.messenger = messenger;
+    }
+
+    @GetMapping
+    public ResponseEntity<String> verifyWebhook(@RequestParam(MODE_REQUEST_PARAM_NAME) final String mode,
+            @RequestParam() final String verifyToken,
+            @RequestParam() final String challenge) {
+        logger.debug("Received Webhook verification request - mode: {} | verifyToken:{} | challenge: {}", mode,
+                verifyToken, challenge);
+        try {
+            this.messenger.verifyWebhook(mode, verifyToken);
+            return ResponseEntity.ok(challenge);
+        } catch (MessengerVerificationException e) {
+            logger.warn("Webhook verification failed: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+    }
+
+    @PostMapping
     public ResponseEntity<String> handleWebhook(@RequestBody String payload) {
         System.out.println("Inpunt: " + payload);
         try {
